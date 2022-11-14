414ba73e0925e193ca2947d2ab1aff81ae86eb93
Ma Pengfei <115222128+shaoyaoqian@users.noreply.github.com>
2022-11-14 23:44:26 +0800

    。

--- douban.py
+++ douban.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import random
+import string
 
 from utils import email
 from database import *
@@ -28,7 +30,8 @@ def analysis_movie(soup):
     kinds_temp    = soup.findAll(name='span', attrs={"property" :"v:genre"})
     kinds = [kind.string for kind in kinds_temp]
     # 查找国家
-    country = soup.find_all(name='span',text='制片国家/地区:')[0].next_sibling
+    country = soup.find_all(name='span',text='制片国家/地区:')
+    country = ' ' if len(country) == 0 else country[0].next_sibling
     # 拼接副标题
     subtitle = " {} / {} / {} / {} ".format(
         ",".join(country.split(" / ")), 
@@ -60,7 +63,7 @@ def find_movie_recommendations(soup):
     return all_subject_id
 
 
-def fetch_one_subject(timeout=5):
+def fetch_one_subject(timeout=20):
     # 从数据库中选取1条未爬取的数据
     # 如果数据库中没有爬取过的数据，那么生成一个7位数的随机数
     result = curs.execute(SELECT_STATUS.format(status=0))
@@ -71,13 +74,17 @@ def fetch_one_subject(timeout=5):
         seed = 1292403
 
     url = url_base + str(seed)
+    print(url)
 
     # 请求数据，如果请求失败，停止此次任务，下次继续。
+    headers['Cookie']=Cookie.format(bid = "bid=%s" % "".join(random.sample(string.ascii_letters + string.digits, 11)))
+    # headers['Cookie'] = ''
     try: 
         response_movie = requests.get(url=url, headers=headers, timeout=timeout, proxies=proxies)
     except:
         exit()
     if response_movie.status_code != 200:
+        print(response_movie)
         exit()
 
     # 初始化soup库
@@ -114,13 +121,14 @@ def fetch_one_subject(timeout=5):
 # 请求头
 url_base = "https://movie.douban.com/subject/"
 headers = {
-    "Cookie": "ll=\"118371\"; bid=ujBcNwnZ9Lc; __utmc=30149280; __utmz=30149280.1668276173.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __gads=ID=b6acfc64c8d441ac-22b7559f40d80002:T=1668276176:RT=1668276176:S=ALNI_MZdL-d_l65y7cW4EOfY4WKGi83LKw; push_noty_num=0; push_doumail_num=0; __yadk_uid=3qqwL8vX4Y42dTT0SerwrQAVqnfcTES4; dbcl2=\"62503351:VHCrjpteg4M\"; ck=4zhh; __utmv=30149280.6250; __gpi=UID=00000b7ae4562923:T=1668276176:RT=1668328812:S=ALNI_MaVl14ymNIBMp9Kma3BBYiS7-E2Cw; _pk_ses.100001.8cb4=*; __utma=30149280.657680781.1668276173.1668331581.1668339742.9; __utmt=1; _pk_id.100001.8cb4=78c23fb0f9637844.1668306835.3.1668339765.1668314288.; __utmb=30149280.4.10.1668339742",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.52"
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.52',
+    'Cookie': "ll=\"118371\"; bid=ujBcNwnZ9Lc; __utmz=30149280.1668276173.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __gads=ID=b6acfc64c8d441ac-22b7559f40d80002:T=1668276176:RT=1668276176:S=ALNI_MZdL-d_l65y7cW4EOfY4WKGi83LKw; push_noty_num=0; push_doumail_num=0; __yadk_uid=3qqwL8vX4Y42dTT0SerwrQAVqnfcTES4; __utmv=30149280.6250; __gpi=UID=00000b7ae4562923:T=1668276176:RT=1668350550:S=ALNI_MaVl14ymNIBMp9Kma3BBYiS7-E2Cw; _pk_id.100001.8cb4=78c23fb0f9637844.1668306835.6.1668434978.1668350547.; _pk_ses.100001.8cb4=*; __utma=30149280.657680781.1668276173.1668350552.1668434981.12; __utmc=30149280; __utmt=1; __utmb=30149280.1.10.1668434981"
 }
+Cookie = "ll=\"118371\"; {bid:s}; __utmz=30149280.1668276173.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __gads=ID=b6acfc64c8d441ac-22b7559f40d80002:T=1668276176:RT=1668276176:S=ALNI_MZdL-d_l65y7cW4EOfY4WKGi83LKw; push_noty_num=0; push_doumail_num=0; __yadk_uid=3qqwL8vX4Y42dTT0SerwrQAVqnfcTES4; __utmv=30149280.6250; __gpi=UID=00000b7ae4562923:T=1668276176:RT=1668350550:S=ALNI_MaVl14ymNIBMp9Kma3BBYiS7-E2Cw; _pk_id.100001.8cb4=78c23fb0f9637844.1668306835.6.1668434978.1668350547.; _pk_ses.100001.8cb4=*; __utma=30149280.657680781.1668276173.1668350552.1668434981.12; __utmc=30149280; __utmt=1; __utmb=30149280.1.10.1668434981"
 
 proxies = {
   'http': 'http://127.0.0.1:7890',
-  'https': 'http://127.0.0.1:7890',
+  'https': 'http://127.0.0.1:7890'
 }
 
 import time
@@ -133,9 +141,6 @@ for i in range(400):
     if i % 20 == 0 :
         email(message,'主题测试','发件人','收件人','mpf_npu@icloud.com')
     fetch_one_subject()
-
-
-
 
 
 result = curs.execute("SELECT id, status  from MOVIE")
