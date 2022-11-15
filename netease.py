efe071e6b4f445a15893aa0ce243e1a500694ce8
Ma Pengfei <115222128+shaoyaoqian@users.noreply.github.com>
2022-11-16 00:27:13 +0800

    更新网易云音乐爬虫

--- netease.py
+++ netease.py
@@ -1,20 +1,90 @@
+
+# 网易云爬虫，使用了NeteaseCloudMusicApi，文档参见：
 # https://neteasecloudmusicapi.vercel.app
-# 网易云爬虫使用了NeteaseCloudMusicApi
-# 调用API的两种方法:
-#
-# 方法一
-import requests
-response = requests.get('http://localhost:3000/song/url?id=208891')
-#
-#
-# 方法二
-import requests
-data = {
-    'id': '436147423'
-}
-response = requests.get('http://localhost:3000/download/song/url', params=data)
+# 第三方API服务搭建在vercel上
 
-with open('a.mp3', 'wb') as f:
-    f.write(response.content)
+import requests
 
-# 我想用爬
+phone    = '15991859247'
+password = 'a12345'
+url_base = "https://netease.pengfeima.cn"
+
+class NeteaseAPI():
+    def login(self):
+        url = url_base+"/login/cellphone"
+        params = {
+            'phone': phone,
+            'password': password
+        }
+        response = requests.get(url,params=params)
+        print(response.url)
+        return response
+
+    def singer(self, id = "11972054"):
+        url = url_base + "/artist/desc"
+        params = {
+            'id':id
+        }
+        response = requests.get(url,params=params)
+        print(response.url)
+        return response
+
+    def singer_details(self, id = "11972054"):
+        url = url_base + "/artist/detail"
+        params = {
+            'id':id
+        }
+        response = requests.get(url,params=params)
+        print(response.url)
+        return response
+
+    def singer_albums(self, id = "11972054", limit=5, offset=0):
+        url = url_base + "/artist/album"
+        params = {
+            'id':id,
+            'limit':limit,
+            'offset':offset
+        }
+        response = requests.get(url,params=params)
+        print(response.url)
+        return response
+
+    def song(self, id = '436147423', level = 'standard'):
+        # url = url_base + "/song/url"          # old API
+        url = url_base + "/song/url/v1"
+        params = {
+            'id'    : id,
+            'level' : level  
+            # standard => 标准
+            # higher   => 较高
+            # exhigh   => 极高
+            # lossless => 无损 
+            # hires    => Hi-Res
+        }
+        response = requests.get(url,params=params)
+        print(response.url)
+        return response
+
+    def song_download(self, id = '1834270728', level = 'standard'):
+        result = self.song(id=id,level=level)
+        # TODO : 判断歌曲是否能下载
+        url_song =  result.json()['data'][0]['url']
+        response = requests.get(url_song)
+        # TODO : 文件名需要外部传入，后缀需要从url解析
+        filename = 'a.mp3'
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+        print(response.url)
+        return response
+
+
+# test 
+NA = NeteaseAPI()
+result = NA.login()
+result = NA.singer()
+result = NA.singer_details()
+result = NA.singer_albums()
+result = NA.song()
+result = NA.song_download()
+
+
